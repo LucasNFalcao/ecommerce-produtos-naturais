9d274ad179fe010a5403ec7880b693524edf5b03
Lucas Falcão <lucasfalcao23@hotmail.com>
2023-11-09 19:15:13 -0300

    feat: Criação do botão Comprar

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,8 +1,8 @@
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native'
 
 import TextComponent from '../../../../src/componentes/TextComponent'
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco }) {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
   return (
     <>
       <TextComponent style={detalhesStyles.nome}>{nome}</TextComponent>
@@ -12,11 +12,21 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
       </View>
       <TextComponent style={detalhesStyles.descricao}>{descricao}</TextComponent>
       <TextComponent style={detalhesStyles.preco}>{preco}</TextComponent>
+
+      <TouchableOpacity style={detalhesStyles.botao} onPress={() => {}}>
+        <TextComponent style={detalhesStyles.textoBotao}>{botao}</TextComponent>
+      </TouchableOpacity>
     </>
   )
 }
 
 const detalhesStyles = StyleSheet.create({
+  botao: {
+    marginTop: 16,
+    backgroundColor: '#2A9F85',
+    paddingVertical: 16,
+    borderRadius: 8,
+  },
   descricao: {
     color: '#A3A3A3',
     fontSize: 16,
@@ -47,5 +57,12 @@ const detalhesStyles = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
+  },
+  textoBotao: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: 'bold',
   },
 })
